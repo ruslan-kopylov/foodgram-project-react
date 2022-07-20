858a0d135f85585f8611f0707630e4f717a21ae0
Руслан Копылов <prbzdj@yandex.ru>
2022-07-20 23:24:02 +0300

    готово к деплою

---
+++ backend/foodgram/recipes/management/commands/loadingredients.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+import json
+from recipes.models import Ingredient
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open('/app/ingredients.json', 'rb') as file:
+            data = json.load(file)
+
+            for item in data:
+                Ingredient(
+                    name = item['name'],
+                    measurement_unit = item['measurement_unit']
+                ).save()
+        return ('Ингредиенты добавлены в базу данных')
